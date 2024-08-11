ff528d7e98695506cfd077a608c011f96d3dc346
Cesar Valencia <cesarvalencia@live.com.mx>
2024-08-11 16:54:40 -0600

    fead: add ES Translations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     <!-- Scripts -->
     @livewireStyles
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
 
 </head>
 
